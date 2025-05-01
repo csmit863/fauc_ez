47d45002cbf7c59db0722d5ab21b0a6514569bec
csmit863 <csmit863@outlook.com>
2025-05-01 22:13:15 +1000

    minor fix

--- ethereum/send_eth.py
+++ ethereum/send_eth.py
@@ -11,17 +11,17 @@ eth_distribution_amount = int(0.452*10**18)
 
 
 async def send_eth(address, rpc):
-
+    print(rpc)
     web3_instance = Web3(HTTPProvider(rpc)) # either sepolia or qut testnet
     # check there is enough to send before attempting, if not, return an error message
-    nonce = web3_instance.eth.get_transaction_count(faucet_account.address)    
+        
     try:
         tx = {
-            'nonce': nonce,
+            'nonce': web3_instance.eth.get_transaction_count(faucet_account.address, "pending"),
             'to': address,
             'value': eth_distribution_amount,
             'gas': 200000,
-            'gasPrice': web3_instance.to_wei(50, 'gwei')
+            'gasPrice': web3_instance.to_wei(60, 'gwei')
         }
         signed_tx = web3_instance.eth.account.sign_transaction(tx, private_key)
         tx_hash = web3_instance.eth.send_raw_transaction(signed_tx.raw_transaction)
